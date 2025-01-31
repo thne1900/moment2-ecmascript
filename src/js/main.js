789bf2364903f4b5969e75ed3e776fd023fdbfa1
Therese Nevander <thne1900@student.miun.se>
2025-02-01 00:00:04 +0100

    Påbörjat arbete i js-filen med FetchAPI. Utformat html-dokumentet.

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let courses=[];
+window.onload=()=>{
+    loadCourses();
+}
+async function loadCourses() {
+    try {
+        const response=await fetch("https://webbutveckling.miun.se/files/ramschema_ht24.json");
+        if(!response.ok){
+            throw new Error("Fel vid datainhämtningen");
+        }
+        courses=await response.json();
+        
+    } catch (error) {
+        console.error(error);
+       document.querySelector("#fail").innerHTML="<p>Tyvärr kan inte innehållet visas för tillfället.</p>"; 
+    }
+}
