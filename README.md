dfbdecded612b8d33e92d0f8db1ee19d63348625
Therese Nevander <thne1900@student.miun.se>
2025-02-05 09:03:14 +0100

    Fortsatt arbete i js-filen med att få ut datan till tabell.

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 ## Skapa en kurstabell.
 
 - Startade upp ett projekt med de grundfiler som jag använde mig av i Moment 1. 
-- Skapade de filer som behövdes; html, css, js. 
-- Skapade ett repo på GitHub för versionshantering. 
--Testkörde kommandon.
+- Skapade de filer som behövdes till detta moment; html, css, js. 
+- Skapade ett repo på GitHub för versionshantering med samma namn. 
+- Testkörde kommandon och kontrollerade så att det blev synkade till Netlify för publicering.
 - Lade filer som inte ska versionshanteras i .gitignore.
+- Började initialt att koda i js-filen kring fetch-API, async/await och try/catch för att inhämta den data som ska användas. 
+- Efter det ska datan placeras i en tabell.
--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@
 let courses=[];
 window.onload=()=>{
     loadCourses();
+
 }
 async function loadCourses() {
     try {
@@ -11,9 +12,55 @@ async function loadCourses() {
             throw new Error("Fel vid datainhämtningen");
         }
         courses=await response.json();
+        displayCourses(courses)
         
     } catch (error) {
         console.error(error);
        document.querySelector("#fail").innerHTML="<p>Tyvärr kan inte innehållet visas för tillfället.</p>"; 
     }
 }
+
+function displayCourses(data){
+   
+const codeElement=document.getElementById("code");
+courses.forEach(courses=>{
+    const row=document.createElement("tr");
+    const codeRow=document.createElement("td");
+    codeRow.innerHTML+=courses.code;
+
+    row.appendChild(codeRow);
+    codeElement.appendChild(row);
+    
+    data.sort((a, b)=> a.code > b.code ? 1:-1);
+    
+})
+    const nameElement=document.getElementById("name");
+    courses.forEach(courses=>{
+        const row=document.createElement("tr");
+        const nameRow=document.createElement("td");
+        nameRow.innerHTML+=courses.coursename;
+    
+        row.appendChild(nameRow);
+        nameElement.appendChild(row);
+        
+        data.sort((a, b)=> a.coursename > b.coursename ? 1:-1);
+
+})
+const progElement=document.getElementById("prog");
+courses.forEach(courses=>{
+    const row=document.createElement("tr");
+    const progRow=document.createElement("td");
+    progRow.innerHTML+=courses.progression;
+
+    row.appendChild(progRow);
+    progElement.appendChild(row);
+
+    data.sort((a, b)=> a.progression > b.progression ? 1:-1);
+
+})
+}
+
+
+
+
+
