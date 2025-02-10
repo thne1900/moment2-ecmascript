0eb7d41495436502908dc9ecaef6002924faa016
Therese Nevander <thne1900@student.miun.se>
2025-02-10 12:47:34 +0100

    Lade till kommentarer i js-filen.

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
 "use strict";
-
+//Creates an empty array here
 let courses=[];
 
 window.onload=()=>{
     loadCourses();
-
+//Adds event handlers to headers and input fields.
 document.getElementById("code").addEventListener("click", sortCode);
 document.getElementById("name").addEventListener("click", sortName);
 document.getElementById("prog").addEventListener("click", sortProg);
 document.getElementById("filter").addEventListener("input", filterCourses);
 }
-
+//Function containing FetchAPI along with async/await and try/catch.
 async function loadCourses() {
     try {
         const response=await fetch("https://webbutveckling.miun.se/files/ramschema_ht24.json");
@@ -25,7 +25,7 @@ async function loadCourses() {
        document.querySelector("#fail").innerHTML="<p>Tyvärr kan inte innehållet visas för tillfället.</p>"; 
     }
 }
-
+//Function that outputs the data in a table on the page.
 function displayCourses(courses){
    
 const tbodyEl=document.getElementById("table");
@@ -36,6 +36,8 @@ courses.forEach((course)=>{
     
 });
 }
+
+//Sorting functions for the data.
 function sortCode() {
         courses.sort((a, b)=> a.code > b.code ? 1:-1);
 
@@ -54,11 +56,13 @@ function sortProg() {
     displayCourses(courses);
 }
 
+//Filter function for the data when entering text in the input field.
 function filterCourses() {
     const filterText=document.getElementById("filter").value;
 
 const coursesFiltered=courses.filter((course)=>
-    course.coursename.toLowerCase().includes(filterText.toLowerCase())
+    course.coursename.toLowerCase().includes(filterText.toLowerCase()) ||
+    course.code.toLowerCase().includes(filterText.toLowerCase())
 );
 displayCourses(coursesFiltered);
 }
